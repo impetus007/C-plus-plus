90b98e0bfa49cc78625e6b8d648f4bb9dbf9b668
impetus007 <vishal2219singh@gmail.com>
2021-08-26 20:16:54 +0530

    updated

--- code.cpp
+++ code.cpp
@@ -1,8 +1,68 @@
+//************jai shree krishna**********
 //write a program to print hello world
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"hello world";
+//     return 0;
+// }
 
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"hello world";
-    return 0;
-}
+
+
+
+//****************decision making if/else*****************
+
+//write a program to find a person eligible for vote or not
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,age=18;
+//     cout<<"Enter your age : " ;
+//     cin>>a;
+//     if(a>age){
+//         cout<<"congratulation! you are eligible for vote" <<endl;
+//     }else{
+//         cout<<"aww aww.. sorry you are not eligible for vote"<<endl;
+//     }
+//     return 0;
+// }
+
+
+//***************else if**************
+
+//write a program to compare two numbers.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x,y;
+//     cout<<"Enter a first no :" ;
+//     cin>>x;
+//     cout<<"Enter a second no :";
+//     cin>>y;
+//     if(x>y){
+//         cout<<"first no is greater than second and its value is "<<x;
+//     }
+//     else if(x==y){
+//         cout<<"Both no are equal and their value is " <<x;
+//     }else{
+//         cout<<"Second no is greater than first and it's value is "<<y;
+//     }
+//     return 0;
+// }
+
+
+// ************for loop**************
+
+// write a program to find out the sum of n no
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n , sum=0;
+//     cout<<"Enter a no : ";
+//     cin>>n;
+//     for(int i=0;i<=n;i++){
+//         sum=sum+i;
+//     }
+//     cout << "Sum of no is :"<<sum ;
+//     return 0;
+// }
