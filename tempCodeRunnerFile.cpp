7ec75a10d5f638debd4608a680c78cb8b3eb18c4
impetus007 <vishal2219singh@gmail.com>
2022-03-26 10:42:45 +0530

    linkedlist

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,15 @@
- int pivotindex = start + count;
-    int temp = arr[start];
-    arr[start] = arr[pivotindex];
-    arr[pivotindex] = temp;
+#include <bits/stdc++.h>
+#define int long long
+#define ull unsigned long long
+using namespace std;
+signed main()
+{
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      int a = 3, b = 4;
+      cout << a << " " << b;
+   }
+   return 0;
+}
